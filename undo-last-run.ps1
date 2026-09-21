4abb90c1a2299251bfc9ace84d9944232fe5204f
Łaurent ʘ❢Ŧ Ŧough <LaurentFough@users.noreply.github.com>
2026-09-21 15:44:18 -0700

    Update script header comments for accuracy

--- undo-last-run.ps1
+++
@@ -1,3 +0,0 @@
-# This file is generated automatically by disable-services.ps1.
-# The actual undo script is regenerated after every optimization run.
-# Run undo-last-run.ps1 as Administrator to restore the previous run.
---
+++ win11-service-undo-last-run.ps1
@@ -0,0 +1,3 @@
+# This file is generated automatically by win11-service-disable.ps1.
+# The actual undo script is regenerated after every optimization run.
+# Run win11-service-undo-last-run.ps1 as Administrator to restore the previous run.
